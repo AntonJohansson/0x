b8556f0725abb546a922b026e5e58a3f4d5a70d6
= <97antjoh@gmail.com>
2019-03-14 02:43:21 +0100

    no longer requires input every turn + hex data retrieval works correctly

--- python_client/client.py
+++ python_client/client.py
@@ -14,6 +14,9 @@ sock.setblocking(0)
 
 #sock.send(bytes("create_or_join observer hej 4", 'utf8'))
 
+data = input()
+sock.send(bytes(data, 'utf8'))
+
 def create_transfer_packet(res,q0,r0,q1,r1):
     return struct.pack('>Biiiii', 3, res,q0,r0,q1,r1)
 
@@ -25,23 +28,21 @@ def hex_neighbours(data, i):
 
 
 def interpret_turn_data(data):
-    print(len(data)/4)
+    #print(len(data)/4)
     unpacked_data = struct.unpack('>' + str(int(len(data)/4)) + 'i', data)
+    print(len(unpacked_data)/(28))
     return unpacked_data, len(unpacked_data)/(SIZE_PER_HEX*7)
 
 while 1:
-    data = input()
-    sock.send(bytes(data, 'utf8'))
-
     try:
-        data = sock.recv(1024)
+        data = sock.recv(4096)
     except:
         continue
 
     print("{} bytes received".format(len(data)))
     U, i = interpret_turn_data(data)
     random_hex_id = randint(0, i-1)
-    first_hex = hex(U, random_hex_id)
+    first_hex = hex(U, random_hex_id*7)
 
     n,l = hex_neighbours(U, random_hex_id)
     random_new_hex_id = randint(0,l-1)
@@ -49,7 +50,7 @@ while 1:
 
     print(random_hex_id)
     print(first_hex)
-    transfer_amount = randint(0,first_hex[3])
+    transfer_amount = randint(1,first_hex[3])
 
     print("Transferring {} from ({},{}) to ({},{})".format(transfer_amount, first_hex[0], first_hex[1], new_hex[0], new_hex[1]))
     b = create_transfer_packet(transfer_amount, first_hex[0],first_hex[1],new_hex[0],new_hex[1])
