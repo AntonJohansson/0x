492dd0823473fe3bc8b0c7b6b3a4ca3a69158556
= <97antjoh@gmail.com>
2019-03-14 06:27:20 +0100

    testing mulitple games

--- game_test.sh
+++ game_test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-n=5
+n=2
 
 termite -e "./build/0x/0x"&
 sleep 0.1
 for i in $(seq 1 $n); do
-	echo $i
 	termite -e "python python_client/client.py coj p hej $n"&
+done
+for i in $(seq 1 $n); do
+	termite -e "python python_client/client.py coj p tset $n"&
 done
